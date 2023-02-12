1519c6aa423f4d25e47cdae50d8cd24be441aadf
Anwarul Islam <anwaarulislaam@gmail.com>
2023-02-12 21:32:22 +0600

    feat: auth and home page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { AppRoutingModule } from './app.routes';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { MainComponent } from './layout/main/main.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, MainComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
