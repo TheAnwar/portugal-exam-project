8bf7fc9cd2a2d0e2cff16fedf18542c7fa402dc5
Anwarul Islam <anwaarulislaam@gmail.com>
2023-03-13 00:36:16 +0530

    feat: correct answer

--- src/app/pages/exam/modalities/modality-emj/modality-emj.component.ts
+++ src/app/pages/exam/modalities/modality-emj/modality-emj.component.ts
@@ -8,6 +8,27 @@ import { Component, Input } from '@angular/core';
 export class ModalityEmjComponent {
   @Input() isSubmitted = false;
 
+  questions = [
+    {
+      id: '1',
+      title:
+        '(A) No seculo passado, a Fundacao Nobel ____________ dois premios a Marie Curie',
+      options: ['ans 1', 'ans 2', 'ans 3 '],
+    },
+  ];
+
+  answers: any = {};
+
+  correctAns: any = {
+    '1': 0,
+    '2': 2,
+    '3': 1,
+  };
+
+  setAnswer(qid: any, oidx: number) {
+    this.answers[qid] = oidx;
+  }
+
   show = false;
 
   showToggle() {
--- src/app/pages/exam/modalities/modality-emm/modality-emm.component.ts
+++ src/app/pages/exam/modalities/modality-emm/modality-emm.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modality-emm',
   templateUrl: './modality-emm.component.html',
-  styleUrls: ['./modality-emm.component.scss']
+  styleUrls: ['./modality-emm.component.scss'],
 })
 export class ModalityEmmComponent {
+  @Input() isSubmitted = false;
 
+  questions = [
+    {
+      id: '1',
+      title:
+        '(A) No seculo passado, a Fundacao Nobel ____________ dois premios a Marie Curie',
+      options: ['ans 1', 'ans 2', 'ans 3 '],
+    },
+    {
+      id: '2',
+      title: 'lorem ipsum?',
+      options: ['ans 1', 'ans 2', 'ans 3 '],
+    },
+    {
+      id: '3',
+      title: 'this is question 2',
+      options: ['ans 1', 'ans 2', 'ans 3 '],
+    },
+  ];
+
+  answers: any = {};
+
+  correctAns: any = {
+    '1': 0,
+    '2': 2,
+    '3': 1,
+  };
+
+  setAnswer(qid: any, oidx: number) {
+    this.answers[qid] = oidx;
+  }
 }
