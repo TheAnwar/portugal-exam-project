d590a42be43b4dade7e5cf6fe1e264baf350a4c0
Anwarul Islam <anwaarulislaam@gmail.com>
2023-03-28 14:24:54 +0600

    fix: em modality

--- src/app/pages/exam/exam.component.ts
+++ src/app/pages/exam/exam.component.ts
@@ -22,7 +22,7 @@ export class ExamComponent {
     { name: 'EQ', code: 'eq' },
   ];
 
-  currentModality = 'emm';
+  currentModality = 'em';
   submitted: boolean = false;
   isValid: boolean = false;
 
